9fd07b61144cbdf24d25a534c4350568afb7141d
test <erickoboae@gmail.com>
2026-05-11 01:35:44 -0500

    add likebutton

--- scripts/index.js
+++ scripts/index.js
@@ -32,22 +32,6 @@ const cardTemplate = document
   .querySelector("#card-template")
   .content.querySelector(".card");
 
-function getCardElement(title, imageUrl) {
-  const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector(".card__title").textContent = title;
-  cardElement.querySelector(".card__image").src = imageUrl;
-  cardElement.querySelector(".card__image").alt = title;
-  return cardElement;
-}
-
-function renderCard(title, imageUrl) {
-  const cardElement = getCardElement(title, imageUrl);
-  cardsContainer.appendChild(cardElement);
-}
-
-initialCards.forEach((card) => {
-  renderCard(card.name, card.link);
-});
 ////////////////////////////////////////////////////////////////////
 // Proyecto de sprint 6: Etapa 2. Perfil modal
 
@@ -58,6 +42,10 @@ const nameInput = editPopup.querySelector(".popup__input_type_name");
 const descriptionInput = editPopup.querySelector(
   ".popup__input_type_description",
 );
+const imagePopup = document.querySelector("#image-popup");
+const popupImage = imagePopup.querySelector(".popup__image");
+const popupCaption = imagePopup.querySelector(".popup__caption");
+const closeImagePopupButton = imagePopup.querySelector(".popup__close");
 
 function fillProfileForm() {
   nameInput.value = document.querySelector(".profile__title").textContent;
@@ -76,7 +64,7 @@ editButton.addEventListener("click", handleOpenEditPopup);
 let formElement = document.querySelector("#edit-profile-form");
 
 function handleProfileFormSubmit(event) {
-  event.preventDefault();
+  event.preventDefault("submit");
   document.querySelector(".profile__title").textContent = nameInput.value;
   document.querySelector(".profile__description").textContent =
     descriptionInput.value;
@@ -87,6 +75,84 @@ formElement.addEventListener("submit", handleProfileFormSubmit);
 
 closeEditPopupButton.addEventListener("click", function () {
   editPopup.classList.remove("popup_is-opened");
+});
+
+//////////////////////////////////////////////////////////////////////////////////7
+// Proyecto de sprint 6: Etapa 3. Tarjetas/////////
+function getCardElement(
+  title = "Sin título",
+  imageUrl = "./images/placeholder.jpg",
+) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardElement.querySelector(".card__title").textContent = title;
+  cardImage.src = imageUrl;
+  cardImage.alt = title;
+
+  cardImage.addEventListener("click", () => {
+    popupCaption.textContent = title;
+    popupImage.src = imageUrl;
+    popupImage.alt = title;
+    openModal(imagePopup);
+  });
+
+  const likeButton = cardElement.querySelector(".card__like-button");
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  return cardElement;
+}
+
+function renderCard(title, imageUrl) {
+  const cardElement = getCardElement(title, imageUrl);
+  cardsContainer.appendChild(cardElement);
+}
+
+initialCards.forEach((card) => {
+  renderCard(card.name, card.link);
+});
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+const addButton = document.querySelector(".profile__add-button");
+const cardPopup = document.querySelector("#new-card-popup");
+const closeCardPopupButton = cardPopup.querySelector(".popup__close");
+function handleCardFormSubmit() {
+  const titleInput = document.querySelector(".popup__input_type_card-name");
+  const imageUrlInput = document.querySelector(".popup__input_type_url");
+  renderCard(titleInput.value, imageUrlInput.value);
+  closeModal(cardPopup);
+  titleInput.value = "";
+  imageUrlInput.value = "";
+}
+
+closeImagePopupButton.addEventListener("click", () => {
+  closeModal(imagePopup);
+});
+
+closeCardPopupButton.addEventListener("click", () => {
+  closeModal(cardPopup);
+});
+addButton.addEventListener("click", () => {
+  openModal(cardPopup);
+});
+formElement = document.querySelector("#new-card-form");
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleCardFormSubmit();
 });
 
 //muchas gracias por la revisión y los comentarios.
